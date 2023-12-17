8be9004f8a29c5d6f0bc6842427a99e317969f4b
Dmitry Shishkin <79608355+ihopeyoucanfly@users.noreply.github.com>
2023-12-17 21:26:54 +0300

    initial version

---
+++ index.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const sequelize = require("./db");
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => console.log(`server started on port ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();

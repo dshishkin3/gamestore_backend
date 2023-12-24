6d5732f1358a78740d07805152e411591ede59f2
zamukaev <abu.safiia2016@mail.com>
2023-12-24 14:42:05 +0100

    add routes:addToFavorites, addToBasket, removeFromBasket,removeFromFavorites

--- controllers/user-controller.ts
+++ controllers/user-controller.ts
@@ -7,70 +7,70 @@ import userService from "../service/user-service";
 import { LoginRequestBody, RegistrationRequestBody } from "../models/types";
 
 class UserController {
-  async registration(
-    req: Request<{}, {}, RegistrationRequestBody>,
-    res: Response,
-    next: NextFunction
-  ): Promise<any> {
-    try {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return next(ApiError.BadRequest("Validation error", errors.array()));
-      }
+    async registration(
+        req: Request<{}, {}, RegistrationRequestBody>,
+        res: Response,
+        next: NextFunction
+    ): Promise<any> {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return next(ApiError.BadRequest("Validation error", errors.array()));
+            }
 
-      const { number, password, username } = req.body;
-      const userData = await userService.registration(
-        number,
-        password,
-        username
-      );
-      res.cookie("refreshToken", userData.refreshToken, {
-        maxAge: 30 * 24 * 60 * 60 * 1000,
-        httpOnly: true,
-      });
-      res.json(userData);
-    } catch (e) {
-      next(e);
+            const { number, password, username } = req.body;
+            const userData = await userService.registration(
+                number,
+                password,
+                username
+            );
+            res.cookie("refreshToken", userData.refreshToken, {
+                maxAge: 30 * 24 * 60 * 60 * 1000,
+                httpOnly: true,
+            });
+            res.json(userData);
+        } catch (e) {
+            next(e);
+        }
     }
-  }
 
-  async login(
-    req: Request<{}, {}, LoginRequestBody>,
-    res: Response,
-    next: NextFunction
-  ): Promise<any> {
-    try {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res
-          .status(400)
-          .json({ error: "Проверьте корректность введенных данных!" });
-      }
+    async login(
+        req: any,
+        res: Response,
+        next: NextFunction
+    ): Promise<any> {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res
+                    .status(400)
+                    .json({ error: "Проверьте корректность введенных данных!" });
+            }
 
-      const { number, password } = req.body;
-      const userData = await userService.login(number, password);
-      res.cookie("refreshToken", userData.refreshToken, {
-        maxAge: 30 * 24 * 60 * 60 * 1000,
-        httpOnly: true,
-      });
-      res.json(userData);
-    } catch (e) {
-      next(e);
+            const { number, password } = req.body;
+            const userData = await userService.login(number, password);
+            res.cookie("refreshToken", userData.refreshToken, {
+                maxAge: 30 * 24 * 60 * 60 * 1000,
+                httpOnly: true,
+            });
+            res.json(userData);
+        } catch (e) {
+            next(e);
+        }
     }
-  }
 
-  async logout(req: Request<{}, {}, LoginRequestBody>,
-    res: Response,
-    next: NextFunction) {
-    try {
-      const { refreshToken } = req.cookies;
-      const token = await userService.logout(refreshToken);
-      res.clearCookie("refreshToken");
-      return res.json(token);
-    } catch (e) {
-      next(e);
+    async logout(req: Request<{}, {}, LoginRequestBody>,
+        res: Response,
+        next: NextFunction) {
+        try {
+            const { refreshToken } = req.cookies;
+            const token = await userService.logout(refreshToken);
+            res.clearCookie("refreshToken");
+            return res.json(token);
+        } catch (e) {
+            next(e);
+        }
     }
-  }
 }
 
 export default new UserController();
--- service/user-service.ts
+++ service/user-service.ts
@@ -8,61 +8,57 @@ import tokenService from "./token-service";
 const ApiError = require("../exceptions/api-error");
 
 class UserService {
-  async registration(
-    number: string,
-    password: string,
-    username: string
-  ): Promise<any> {
-    const candidate = await UserModel.findOne({ number });
-    if (candidate) {
-      throw ApiError.BadRequest(
-        `Пользователь с номером ${number} уже существует`
-      );
+    async registration(
+        number: string,
+        password: string,
+        username: string
+    ): Promise<any> {
+        const candidate = await UserModel.findOne({ number });
+        if (candidate) {
+            throw ApiError.BadRequest(
+                `Пользователь с номером ${number} уже существует`
+            );
+        }
+        const hashPassword = await bcrypt.hash(password, 3);
+        const user = await UserModel.create({
+            number,
+            username,
+            favorites: [],
+            password: hashPassword,
+            basket: [],
+        });
+        const userDto = new UserDto(user);
+        const tokens = tokenService.generateTokens({ ...userDto });
+
+        await tokenService.saveToken(userDto.id, tokens.refreshToken);
+        return { ...tokens, user: userDto };
     }
 
-    const hashPassword = await bcrypt.hash(password, 3);
-    const user = await UserModel.create({
-      number,
-      username,
-      favorites: [],
-      password: hashPassword,
-      basket: [],
-    });
+    async login(number: string, password: string): Promise<any> {
+        try {
+            const user = await UserModel.findOne({ number });
+            if (!user) {
+                throw ApiError.BadRequest("Пользователь с этим номером не найден");
+            }
 
-    const userDto = new UserDto(user);
-    const tokens = tokenService.generateTokens({ ...userDto });
-    await tokenService.saveToken(userDto.id, tokens.refreshToken);
-    return { ...tokens, user: userDto };
-  }
+            const isPassEquals = await bcrypt.compare(password, user.password);
+            if (!isPassEquals) {
+                throw ApiError.BadRequest("Неверный пароль");
+            }
 
-  async login(number: string, password: string): Promise<any> {
-    try {
-      const user = await UserModel.findOne({ number });
-      if (!user) {
-        throw ApiError.BadRequest("Пользователь с этим номером не найден");
-      }
-
-      const isPassEquals = await bcrypt.compare(password, user.password);
-      if (!isPassEquals) {
-        throw ApiError.BadRequest("Неверный пароль");
-      }
-
-      const userDto = new UserDto(user);
-
-      const tokens = tokenService.generateTokens({ ...userDto });
-
-      await tokenService.saveToken(userDto.id, tokens.refreshToken);
-      
-      return {...tokens, user: userDto};
-    } catch (error) {
-      throw ApiError.BadRequest("Ошибка при входе пользователя");
+            const userDto = new UserDto(user);
+            const tokens = tokenService.generateTokens({ ...userDto });
+            await tokenService.saveToken(userDto.id, tokens.refreshToken);
+            return { ...tokens, user: userDto };
+        } catch (error) {
+            throw ApiError.BadRequest("Ошибка при входе пользователя");
+        }
     }
-  }
 
-  async logout(refreshToken: string) {
-    const token = await tokenService.removeToken(refreshToken);
-    return token;
-  }
+    async logout(refreshToken: string) {
+        const token = await tokenService.removeToken(refreshToken);
+        return token;
+    }
 }
 
 export default new UserService();
